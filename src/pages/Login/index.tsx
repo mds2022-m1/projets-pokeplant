169f4fb544d2153b10d3ae9782c9677efba1a834
yurihikari <quangminhjean@gmail.com>
2023-01-16 16:19:32 +0100

    Added Login Page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,18 @@
 // Import all the necessary react bootstrap components for login/register page form
-import { useState } from "react";
-import { FormControl, FormLabel } from "react-bootstrap";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { LoginForm } from "../../components/LoginForm";
+import { LoginRegister } from "../../components/LoginRegister";
+import { useAppSelector } from "../../app/hooks";
+import { RouterPath } from "../../app/router-path";
+import { Navigate } from "react-router-dom";
 
 export function Login() {
+  const session = useAppSelector((state) => state.session.session);
   // Form for logging in or registering (use react-bootstrap)
   return (
     <>
-      <LoginForm></LoginForm>
+      {session !== null ? <Navigate to={RouterPath.home} /> : null}
+      <div className="d-flex align-items-center justify-content-center h-100 w-100">
+        <LoginRegister></LoginRegister>
+      </div>
     </>
   );
 }
