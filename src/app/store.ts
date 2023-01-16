ce3c22650d29a87ad4c4b0a1f264de8eb33035c8
yurihikari <quangminhjean@gmail.com>
2023-01-16 16:20:16 +0100

    Updated Store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user-slice";
 import userCredentialsReducer from "../features/usercredential-slice";
+import sessionReducer from "../features/session-slice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         userCrendentials: userCredentialsReducer,
+        session: sessionReducer,
     },
 });
 export type AppDispatch = typeof store.dispatch;
