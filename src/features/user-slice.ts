6ea5f8114f5480230e75c42450fffc4641bb208a
yurihikari <quangminhjean@gmail.com>
2023-01-16 16:23:57 +0100

    Added gender in user slice

--- src/features/user-slice.ts
+++ src/features/user-slice.ts
@@ -1,37 +1,48 @@
 // DUCKS pattern
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
 
 export interface User {
-  id?: string;
-  name?: string;
-  email?: string;
+    id?: string;
+    name?: string;
+    gender?: "Male" | "Female";
 }
 
-const initialState: User =
-  localStorage.getItem("user") && localStorage.getItem("user") !== "undefined"
-    ? JSON.parse(localStorage.getItem("user") || "{}")
-    : {};
+const initialState: User = {
+    id: "",
+    name: "",
+    gender: "Male",
+};
 
 export const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    userLoggedIn: (state, action: PayloadAction<User>) => {
-      state.id = action.payload.id;
-      state.name = action.payload.name;
-      state.email = action.payload.email;
-      localStorage.setItem("user", JSON.stringify(state));
-    },
-    userLoggedOut: (state) => {
-      console.log(state);
-      state.id = undefined;
-      state.name = undefined;
-      state.email = undefined;
-      localStorage.setItem("user", JSON.stringify({}));
-    },
-  },
+    name: 'user',
+    initialState,
+    reducers: {
+        userCreated: (state, action: PayloadAction<User>) => {
+            // Timestamp provided by store
+            const user : User = {
+                ...action.payload,
+                id: uuidv4(),
+            }
+            state.id = user.id;
+            state.name = user.name;
+            state.gender = user.gender;
+        },
+        userIdChanged: (state, action: PayloadAction<string>) => {
+            state.id = action.payload;
+        },
+        userNameChanged: (state, action: PayloadAction<string>) => {
+            state.name = action.payload;
+        },
+        userGenderChanged: (state, action: PayloadAction<"Male" | "Female">) => {
+            state.gender = action.payload;
+        },
+    }
 });
 
-export const { userLoggedIn, userLoggedOut } = userSlice.actions;
+export const { userCreated, userNameChanged, userIdChanged, userGenderChanged } = userSlice.actions;
 
 export default userSlice.reducer;
+
+// Path: src/features/chat/chat.tsx
