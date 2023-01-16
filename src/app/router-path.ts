d5fd09fabc5959a13fb6bfaf332fcc5d57a8fd0e
yurihikari <quangminhjean@gmail.com>
2023-01-16 16:20:28 +0100

    Added Router Path for easier route naming

---
+++ src/app/router-path.ts
@@ -0,0 +1,11 @@
+export const RouterPath = {
+    home: '/',
+    profile: '/profile',
+    settings: '/settings',
+    login: '/login',
+    account: '/account',
+    passwordRecovery: '/password-recovery',
+    map: '/map',
+    pokedex: '/pokedex',
+    capture: '/capture',
+};
