9394441fa1606d5aa9ca159e7c2970e8cef6dd4a
yurihikari <quangminhjean@gmail.com>
2023-01-16 16:23:48 +0100

    Deprecated File

--- src/utils/LogOut.tsx
+++ src/utils/LogOut.tsx
@@ -1,6 +1,5 @@
 import { getAuth, signOut } from "firebase/auth";
 import { useAppDispatch } from "../app/hooks";
-import { userLoggedOut } from "../features/user-slice";
 export function LogOut() {
     console.log("LogOut");
     
