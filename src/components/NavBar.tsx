924e8a0604c00d754a598e78c27b390e208d977b
yurihikari <quangminhjean@gmail.com>
2023-01-16 16:22:37 +0100

    Updated Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,28 +1,16 @@
-import { getAuth, signOut } from "firebase/auth";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import { auth } from "..";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
-import { userLoggedOut } from "../features/user-slice";
+import { supabase } from "../app/supabaseClient";
+import { userCreated } from "../features/user-slice";
+import { passwordRecoveryUpdated } from "../features/session-slice";
+import { FaUserCircle } from "react-icons/fa";
 
 export function NavBar() {
-  const userId = useAppSelector((state) => state.user.id);
   const dispatch = useAppDispatch();
-
-  function LogOut() {
-    signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-        console.log("Sign-out successful.");
-        dispatch(userLoggedOut());
-      })
-      .catch((error) => {
-        // An error happened.
-        console.log(error);
-      });
-  }
-
+  const session = useAppSelector((state) => state.session.session);
+  const username = useAppSelector((state) => state.user.name);
   return (
-    <Navbar variant="light" fixed="top">
+    <Navbar collapseOnSelect variant="light" fixed="top">
       <Container>
         <Navbar.Brand href="/">
           <img
@@ -32,11 +20,12 @@ export function NavBar() {
             className="d-inline-block align-top"
           />{" "}
         </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/map">Map</Nav.Link>
-            {userId !== undefined && (
+            {session ? (
               <NavDropdown title="Pokeplants" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="/capture">Capture</NavDropdown.Item>
                 <NavDropdown.Item href="/battle">Battle</NavDropdown.Item>
@@ -46,30 +35,45 @@ export function NavBar() {
                 </NavDropdown.Item>
                 <NavDropdown.Item href="/teams">Teams</NavDropdown.Item>
               </NavDropdown>
-            )}
+            ) : null}
           </Nav>
           <Nav>
-            <NavDropdown title="Account" id="collasible-nav-dropdown">
-              {userId === undefined ? (
+            <NavDropdown
+              title={
+                username === "" ? (
+                  <>
+                    <FaUserCircle size={30} />
+                  </>
+                ) : (
+                  <>
+                    <FaUserCircle size={30} />
+                    <span> {username}</span>
+                  </>
+                )
+              }
+              id="collasible-nav-dropdown"
+            >
+              {!session ? (
                 <>
                   <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-                  <NavDropdown.Item href="/register">Register</NavDropdown.Item>
                 </>
-              ) : null}
-              {userId !== undefined ? (
+              ) : (
                 <>
-                  <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
-                  <NavDropdown.Item href="/settings">Settings</NavDropdown.Item>
+                  <NavDropdown.Item href="/account">Profile</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item
                     href="#"
                     className="text-danger"
-                    onClick={LogOut}
+                    onClick={() => {
+                      supabase.auth.signOut();
+                      dispatch(userCreated({ name: "", id: "" }));
+                      dispatch(passwordRecoveryUpdated(false));
+                    }}
                   >
                     Log Out
                   </NavDropdown.Item>
                 </>
-              ) : null}
+              )}
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
