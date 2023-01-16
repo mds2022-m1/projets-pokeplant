621d47666b8e7dd36b2b765273bf8afc7d54f8eb
yurihikari <quangminhjean@gmail.com>
2023-01-16 16:21:44 +0100

    Added placeholder type file

---
+++ src/app/types.ts
@@ -0,0 +1,8 @@
+
+
+// export type LocalStreamsType = { [key: string]: MediaStream | null; };
+// export type RemoteStreamsType = { [key: string]: MediaStream | null; };
+
+
+// Repository of MediaStreams received from other peers
+export type test = { test : string }
