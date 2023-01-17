02b32ca5c5210d04b5ea1ac4685d8b7dc4cae478
yurihikari <quangminhjean@gmail.com>
2023-01-17 16:59:30 +0100

    .

--- client/src/pages/Capture/index.tsx
+++ client/src/pages/Capture/index.tsx
@@ -14,6 +14,7 @@ import { AddPokePlant } from "../../components/AddPokePlant";
 export function Capture() {
   const userId = useAppSelector((state) => state.user.id);
   const session = useAppSelector((state) => state.session.session);
+  
   return (
     <>
       {session !== null ? (<AddPokePlant></AddPokePlant>) : (<NotFound></NotFound>)}
