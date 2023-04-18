e8be6d97015550739c1ff10cd497bb3470845299
LightCrimson <quangminhjean@gmail.com>
2023-04-18 12:22:48 +0200

    Removed session checking

--- client/src/components/AddPokePlant.tsx
+++ client/src/components/AddPokePlant.tsx
@@ -1,26 +1,28 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Container, Row } from "react-bootstrap";
 
 import CloudinaryUploadWidget from "./CloudinaryUploadWidget";
-import { useAppSelector } from "../app/hooks";
 
 export function AddPokePlant() {
-  const session = useAppSelector((state) => state.session.session)
   return (
-    <div className="App">
-      <h3>Cloudinary Upload Widget Example</h3>
-      <CloudinaryUploadWidget />
-
-      <Card style={{ width: "18rem" }}>
-        <Card.Img id="uploadedimage" variant="top" src="" />
-        <Card.Body>
-          <Card.Title id="plantName">Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </div>
+    <Container>
+      <Row>
+        <h3>Capture</h3>
+        <CloudinaryUploadWidget />
+      </Row>
+      <Row className="text-center">
+        <Card>
+          <Card.Img id="uploadedimage" variant="top" src="" />
+          <Card.Body>
+            <Card.Title id="plantName">Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Row>
+      
+    </Container>
   );
 }
