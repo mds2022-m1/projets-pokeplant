bcaf490c2ca422e21b3bab81fcb09616a6b53540
LightCrimson <quangminhjean@gmail.com>
2023-04-18 12:24:42 +0200

    Removed session condition

--- client/src/pages/Capture/index.tsx
+++ client/src/pages/Capture/index.tsx
@@ -1,23 +1,10 @@
-import {
-  Button,
-  Card,
-  Container,
-  Form,
-  FormControl,
-  FormLabel,
-} from "react-bootstrap";
-import { MusicModal } from "../../components/MusicModal";
-import { useAppSelector } from "../../app/hooks";
-import { NotFound } from "../NotFound";
 import { AddPokePlant } from "../../components/AddPokePlant";
 
 export function Capture() {
-  const userId = useAppSelector((state) => state.user.id);
-  const session = useAppSelector((state) => state.session.session);
-  
+
   return (
     <>
-      {session !== null ? (<AddPokePlant></AddPokePlant>) : (<NotFound></NotFound>)}
+      <AddPokePlant></AddPokePlant>
     </>
   );
 }
