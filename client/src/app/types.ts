8da62e5e716f643832e4732ff3295a5d44136da9
LightCrimson <quangminhjean@gmail.com>
2023-04-18 12:22:11 +0200

    Added Database table description types (only for reference) Co-authored-by: FriiLow <FriiLow@users.noreply.github.com>

--- client/src/app/types.ts
+++ client/src/app/types.ts
@@ -6,3 +6,24 @@
 
 // Repository of MediaStreams received from other peers
 export type test = { test : string }
+
+export interface pokePlant {
+    id: string; // PK
+    created_at: Date;
+    name: string;
+    owner: profiles["id"]; // FK
+    image: string;
+    description: string;
+    moves: string[];
+    type: string;
+}
+
+export interface profiles {
+    id: string; // PK
+    updated_at: Date;
+    username: string; 
+    full_name: string;
+    avatar_url: string;
+    website: string;
+    gender: "Male" | "Female";
+}
