8b5aaaebfbe125d94005d24758fc23a8a15c58fb
LightCrimson <quangminhjean@gmail.com>
2023-04-18 12:23:09 +0200

    Preparing markers for pokeplants

--- client/src/components/LeafletMap.tsx
+++ client/src/components/LeafletMap.tsx
@@ -7,23 +7,43 @@ export function LeafletMap() {
     {
       lat: 45.764043,
       lng: 4.835659,
-      popup: "Example of Plant",
+      popup: (
+        <>
+          <h1>My Pokeplant</h1>
+          <img src="https://www.sciencesetavenir.fr/assets/img/2017/03/31/cover-r4x3w1000-6214a248e6018-la-dionee-attrape-mouches-la-plus-spectaculaire-des-tueuses.jpg" alt="pokeplant-marker" width={"100%"}></img>
+        </>
+      ),
+      iconUrl:
+        "https://icon-library.com/images/marker-icon-png/marker-icon-png-12.jpg",
     },
     {
       lat: 48.8566,
       lng: 2.3522,
       popup: "Hello",
-    }
+      iconUrl:
+        "https://icon-library.com/images/marker-icon-png/marker-icon-png-12.jpg",
+    },
   ]);
   const [center, setCenter] = useState([45.764043, 4.835659]);
   return (
-    <MapContainer center={center as L.LatLngExpression} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={center as L.LatLngExpression}
+      zoom={3}
+      scrollWheelZoom={false}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {markers.map((marker, index) => (
-        <Marker key={index} position={[marker.lat, marker.lng]}>
+        <Marker
+          key={index}
+          position={[marker.lat, marker.lng]}
+          icon={L.icon({
+            iconUrl: marker.iconUrl,
+            iconSize: [35, 41],
+          })}
+        >
           <Popup>{marker.popup}</Popup>
         </Marker>
       ))}
