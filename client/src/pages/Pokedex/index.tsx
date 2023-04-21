c2fcc653cdbda4daceaed421b5ab12efafe13eec
LightCrimson <quangminhjean@gmail.com>
2023-04-21 14:05:00 +0200

    Changed Oak images

--- client/src/pages/Pokedex/index.tsx
+++ client/src/pages/Pokedex/index.tsx
@@ -73,8 +73,10 @@ export default function Pokedex() {
                   </Card.Header>
                   <Card.Body>
                     <img
-                      src="https://archives.bulbagarden.net/media/upload/thumb/8/8b/Red_Green_Prof_Oak.png/150px-Red_Green_Prof_Oak.png"
+                      src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b41ebc74-a9a7-41c3-98c6-4e68c588fca9/ddmvjr1-1b0c02b9-57ff-4481-a644-cd2bad2f2e41.png/v1/fill/w_1280,h_1318/professor_oak_by_captainedwardteague_ddmvjr1-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTMxOCIsInBhdGgiOiJcL2ZcL2I0MWViYzc0LWE5YTctNDFjMy05OGM2LTRlNjhjNTg4ZmNhOVwvZGRtdmpyMS0xYjBjMDJiOS01N2ZmLTQ0ODEtYTY0NC1jZDJiYWQyZjJlNDEucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.5g2vXX_euQjx3m29NKx7RJd-dxvIbwpcDhviOkZNv-A"
                       alt="oak"
+                      width={300}
+                      height={300}
                     ></img>
                     <h3 className="text-dark">
                       Please help Professor Oak by catching some pokeplants!
