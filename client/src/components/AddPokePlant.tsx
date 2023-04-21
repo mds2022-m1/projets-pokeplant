5b0682e32ae1022bf62fb54cae8f2ad340b50ee0
LightCrimson <quangminhjean@gmail.com>
2023-04-21 14:30:50 +0200

    center image

--- client/src/components/AddPokePlant.tsx
+++ client/src/components/AddPokePlant.tsx
@@ -1,4 +1,4 @@
-import { Card, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
 import CloudinaryUploadWidget from "./CloudinaryUploadWidget";
 
@@ -10,7 +10,9 @@ export function AddPokePlant() {
       </Row>
       <Row className="text-center mt-3">
         <Card>
+          <Col className="md-6">
           <Card.Img id="uploadedimage" variant="top" src="" style={{ maxWidth: "20vw"}}/>
+          </Col>
           <Card.Body>
             <Card.Title id="plantName">Pokeplant added will appear here</Card.Title>
           </Card.Body>
